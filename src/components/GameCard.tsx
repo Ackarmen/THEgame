d94d3a4d682e98fa15a93322ebe0e0614ea62e4a
Ackarmen <lyaforis@gmail.com>
2024-01-25 16:49:43 +0300

    Fix issue with platform filter

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,8 +1,8 @@
 import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import getCroppedImageUrl from "../services/image-url";
 import CriticScore from "./CriticScore";
 import PlatformIconList from "./PlatformIconList";
-import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
@@ -16,7 +16,7 @@ function GameCard({ game }: Props) {
         <Heading fontSize="2xl">{game.name}</Heading>
         <HStack justifyContent="space-between">
           <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
+            platforms={game.parent_platforms?.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
