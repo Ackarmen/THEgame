ec05e4dc338e8a96daa07f6ac32074182aec583f
Ackarmen <lyaforis@gmail.com>
2024-02-01 14:14:40 +0300

    Refactor interfaces

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,13 +1,20 @@
-import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid } from "react-icons/fa"
-import { MdPhoneIphone } from "react-icons/md"
-import { SiNintendo } from "react-icons/si"
-import { BsGlobe } from "react-icons/bs"
-import { HStack, Icon } from "@chakra-ui/react"
-import { Platform } from "../hooks/usePlatforms"
-import { IconType } from "react-icons"
+import { HStack, Icon } from "@chakra-ui/react";
+import { IconType } from "react-icons";
+import { BsGlobe } from "react-icons/bs";
+import {
+  FaAndroid,
+  FaApple,
+  FaLinux,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { Platform } from "../entities/Platform";
 
 interface Props {
-  platforms: Platform[]
+  platforms: Platform[];
 }
 
 function PlatformIconList({ platforms }: Props) {
@@ -15,18 +22,20 @@ function PlatformIconList({ platforms }: Props) {
     pc: FaWindows,
     playstation: FaPlaystation,
     xbox: FaXbox,
-    nintendo : SiNintendo,
+    nintendo: SiNintendo,
     mac: FaApple,
     linux: FaLinux,
     ios: MdPhoneIphone,
     web: BsGlobe,
     android: FaAndroid,
-  }
+  };
   return (
     <HStack marginY={1}>
-    {platforms.map(( platform ) => <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500"/>)}
+      {platforms.map((platform) => (
+        <Icon key={platform.id} as={iconMap[platform.slug]} color="gray.500" />
+      ))}
     </HStack>
-  )
+  );
 }
 
-export default PlatformIconList
+export default PlatformIconList;
