b4ce6a857331cf2f23467b381125b3903d13be0d
Ackarmen <lyaforis@gmail.com>
2024-02-01 16:15:56 +0300

    Refactor interfaces

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,19 +1,29 @@
 import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useRef } from "react";
 import { BsSearch } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 import useGameQueryStore from "../store";
 
 function SearchInput() {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
+  const navigate = useNavigate();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if (ref.current) {
+      setSearchText(ref.current.value);
+      navigate("/");
+
+      if (ref.current.form) {
+        ref.current.form.reset();
+      }
+    }
+  };
 
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-        if (ref.current) setSearchText(ref.current.value);
-      }}
-    >
+    <form onSubmit={handleSubmit}>
       <InputGroup>
         <InputLeftElement>
           <BsSearch />
